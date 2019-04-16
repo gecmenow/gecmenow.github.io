375ccab6690d0d9144f1252dbf587dddddedb431
gecmenow <37314830+gecmenow@users.noreply.github.com>
2019-04-16 22:33:49 +0300

    added js to slider

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.sixth-screen-left-content').slick(
+		{
+			prevArrow: '<div class="slick-prev"></div>',
+            nextArrow: '<div class="slick-next"></div>',
+			arrows: true,
+			dots: true,
+		}
+		);
+	$('.slick-dots button').html('');
+})
