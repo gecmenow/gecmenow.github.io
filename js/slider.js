bdd0b48d45e04a5de2e4bf96e2cb7f975f834b77
gecmenow <37314830+gecmenow@users.noreply.github.com>
2019-04-18 22:19:41 +0300

    files

--- js/slider.js
+++ js/slider.js
@@ -8,4 +8,43 @@ $(document).ready(function(){
 		}
 		);
 	$('.slick-dots button').html('');
+
+	$('.center').slick({
+		prevArrow: '<div class="slick-prev"></div>',
+        nextArrow: '<div class="slick-next"></div>',
+        arrows: true,
+		lazyLoad: 'ondemand',
+		centerPadding: '60px',
+		slidesToShow: 3,
+		responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: true,
+	       	lazyLoad: 'ondemand',
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: true,
+	       	lazyLoad: 'ondemand',
+	        centerPadding: '40px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+
+	document.getElementsByClassName('slick-active')[3].classList.add('center-active');
+
+	var styleChanging = $('.slick-arrow');
+	styleChanging.click(function()
+	{
+		$('.slick-slide').removeClass('center-active');
+		console.log('huj');
+		document.getElementsByClassName('slick-active')[3].classList.add('center-active');
+	})
 })
