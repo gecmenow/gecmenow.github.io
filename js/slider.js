6ad57b9eed492d9f3d4b80816b8108ddbde9fce5
gecmenow <37314830+gecmenow@users.noreply.github.com>
2019-04-19 22:31:35 +0300

    files

--- js/slider.js
+++ js/slider.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 			dots: true,
 		}
 		);
-	$('.slick-dots button').html('');
 
 	$('.center').slick({
 		prevArrow: '<div class="slick-prev"></div>',
@@ -46,5 +45,28 @@ $(document).ready(function(){
 		$('.slick-slide').removeClass('center-active');
 		console.log('huj');
 		document.getElementsByClassName('slick-active')[3].classList.add('center-active');
-	})
+	});
+
+	$('.ten-screen-slider-for').slick({
+		prevArrow: '<div class="slick-prev"></div>',
+        nextArrow: '<div class="slick-next"></div>',
+	  	slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		infinite: true,
+		asNavFor: '.ten-screen-slider-nav'
+	});
+
+	$('.ten-screen-slider-nav').slick({
+		prevArrow: '<div class="slick-prev"></div>',
+        nextArrow: '<div class="slick-next"></div>',
+	 	slidesToShow: 5,
+		slidesToScroll: 1,
+		asNavFor: '.ten-screen-slider-for',
+		dots: true,
+		focusOnSelect: true,
+	});
+
+	$('.slick-dots button').html('');
 })
